ff710e259d760002f52ad11b654e6fd8a7a63426
ffauvelet <fabien.fauvelet@gmail.com>
2023-01-26 14:31:37 +0100

    Remove teachers mock to access real back-end

--- src/app/teachers/services/teachers.service.ts
+++ src/app/teachers/services/teachers.service.ts
@@ -4,8 +4,8 @@ import {HttpClient} from "@angular/common/http";
 
 interface Teacher {
   id:string;
-  firstName:string;
-  lastName:string;
+  firstname:string;
+  lastname:string;
 }
 interface CreateTeacherCommand {
   firstName:string;
@@ -20,11 +20,12 @@ export class TeachersService {
   constructor(private http: HttpClient) { }
 
   getTeachersList() : Observable<Teacher[]> {
-    return of([
+    return this.http.get<Teacher[]>("/query/teachers");
+    /*return of([
       {id:"bcb4c68e-28bc-462f-a6d1-26a2f7e789c7", firstName:"John", lastName:"Doe"}
-    ]);
+    ]);*/
   }
   addTeacher(createTeacherCommand : CreateTeacherCommand) : Observable<any>{
-    return this.http.post<any>("http://localhost:4200/teachers", createTeacherCommand);
+    return this.http.post<any>("/command/teachers", createTeacherCommand);
   }
 }
