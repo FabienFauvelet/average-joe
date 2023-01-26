5df670e9f5139d6534794322d3e59b9cd1da7529
ffauvelet <fabien.fauvelet@gmail.com>
2023-01-26 14:31:52 +0100

    Remove resources mock to access real back-end

--- src/app/resources/services/resources.service.ts
+++ src/app/resources/services/resources.service.ts
@@ -18,11 +18,12 @@ export class ResourcesService {
   constructor(private http: HttpClient) { }
 
   getResourcesList() : Observable<Resource[]>{
-    return of([
+    return this.http.get<Resource[]>("/query/resources");
+    /*return of([
       {id:"bcb4c68e-28bc-462f-a6d1-26a2f7e789c7", name:"Haltères"}
-    ]);
+    ]);*/
   }
   addResource(createResourceCommand : CreateResourceCommand) : Observable<any> {
-    return this.http.post<any>("http://localhost:4200/resources", createResourceCommand);
+    return this.http.post<any>("/command/resources", createResourceCommand);
   }
 }
